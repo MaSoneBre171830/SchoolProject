b86b5fb3f9d4e56505d9c0bfe8e0bbd52c48283f
Nenad <76172679+MaSoneBre171830@users.noreply.github.com>
2024-02-04 22:34:57 +0100

    Add files via upload

---
+++ C#/Block3/Brutto vs Netto/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class BruttoNetto
+{
+
+    public static void Main()
+    {
+    //Ihr Code
+    start:
+
+        char auswahl;
+        double preis;
+        char yesOrNo;
+
+        do
+        {
+            Console.WriteLine("Netto(N) oder Brutto(B)? ");
+            auswahl = Convert.ToChar(Console.ReadLine());
+
+
+        } while (auswahl != 'b' && auswahl != 'B' && auswahl != 'n' && auswahl != 'N');
+
+        Console.WriteLine("Preis: ");
+        preis = Convert.ToDouble(Console.ReadLine());
+
+        if (auswahl == 'b' || auswahl == 'B')
+        {
+            double netto = (preis / 120) * 100;
+            double MWST = (preis / 120) * 20;
+
+            Console.WriteLine($" Netto: {netto} \n +MWST: {MWST}");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Brutto: " + (netto + MWST));
+        }
+        else
+        {
+            double brutto = (preis / 100) * 120;
+            double MWST = (preis / 100) * 20;
+
+            Console.WriteLine($" Netto: {brutto - MWST} \n +MWST: {MWST}");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Brutto: " + (brutto));
+        }
+
+        do
+        {
+            Console.WriteLine("Wollen Sie nochmal(J/N): ");
+            yesOrNo = Convert.ToChar(Console.ReadLine());
+        } while (yesOrNo != 'J' && yesOrNo != 'j' && yesOrNo != 'n' && yesOrNo != 'N');
+
+        if (yesOrNo == 'J' || yesOrNo == 'j')
+        {
+            goto start;
+        }
+
+
+
+    }
+}
---
+++ C#/Block3/DecToHex/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Aufgabe16
+{
+
+    public static void Main()
+    {
+
+        // Schreiben Sie hier Ihren Code
+        long dec;
+        string ant;
+
+        do
+        {
+            string hex = "";
+
+            Console.WriteLine("Dezimalzahl:");
+            dec = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("    Dezimal: " + dec);
+
+            //calculates decimal to hex
+            while (dec > 0)
+            {
+               
+                long temp = dec % 16;
+                switch (temp)
+                {
+                    case 10:
+                        hex += "A";
+                        break;
+                    case 11:
+                        hex += "B";
+                        break;
+                    case 12:
+                        hex += "C";
+                        break;
+                    case 13:
+                        hex += "D";
+                        break;
+                    case 14:
+                        hex += "E";
+                        break;
+                    case 15:
+                        hex += "F";
+                        break;
+                    default:
+                        hex += temp;
+                        break;
+                }
+
+
+
+                dec /= 16;
+            }
+
+            // reverses th string
+            char[] arr = hex.ToCharArray();
+            Array.Reverse(arr);
+            hex = new string(arr);
+
+            Console.WriteLine("Hexadezimal: " + hex);
+
+            Console.WriteLine("Wollen Sie nochmal(J/N):");
+            ant = Console.ReadLine();
+
+        } while (ant == "J" || ant == "j");
+    }
+}
