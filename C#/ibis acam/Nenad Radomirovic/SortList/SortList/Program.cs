b996576831a799568613bddef7e04bbdbb7339c5
Nenad <76172679+MaSoneBre171830@users.noreply.github.com>
2024-02-05 14:17:32 +0100

    Add files via upload

---
+++ C#/ibis acam/Nenad Radomirovic/SortList/SortList/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+        Console.WriteLine("How many numbers do you wanna input?");
+        int nums = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input numbers(one by one): ");
+
+        do
+        {
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            list.Add(numbers);
+                  
+            nums--;
+        } while (nums > 0);
+
+
+        Console.Clear();
+        SortList(list);
+
+        Console.WriteLine("Sorted list: ");   
+
+
+
+        foreach (int num in list)
+            Console.Write(num + ", ");
+    }
+
+    static List<int> SortList(List<int> list)
+    {
+        int ListLength = list.Count; // initializes a variable to make the code cleaner
+        int iritations = 0;
+
+        for(int i = 0; i < ListLength - 1 ; i++) 
+        {
+            for(int j = 0; j < ListLength - i - 1;   j++)
+            {
+                if (list[j] < list[j + 1]) // compares the number at index (j) with number at index j + 1
+                {
+                    // swaps the elements
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                                
+                    iritations++; // counts how many steps it takes to sort list
+                }
+            }
+        }
+
+        Console.WriteLine($"Iritations: {iritations}");
+        // returns the list
+        return list;
+    }
+}
---
+++ C#/ibis acam/NumbersToRomanNumerals/NumbersToRomanNumerals/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ArbeitsAuftrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Restart:
+
+            int Number;
+            //reapeats the asking the user for the "Number" if the number is <= 0
+            do
+            {
+                // asks the user for a number to be converted later
+                Console.Write("Input a number to convert to a Roman numeral: ");
+                Number = Convert.ToInt32(Console.ReadLine());
+
+                // error message if the "Number"  is <= 0
+                if (Number <= 0)
+                {
+                    // the error message
+                    Console.WriteLine("ALOOOO BREE!!!");
+                    Console.WriteLine("Please put a number greater then 0!!");  
+
+                    // clears the console to ask the user again
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+
+            } while (Number <= 0);
+
+            // calls the NumToRoman funktion and prints the result
+            // using the nmber that the user inputed
+            Console.WriteLine(NumToRoman(Number));
+
+            // asks the user if they want to restart the program
+            Console.WriteLine("Do you want to try again? Y/N"); 
+            string input = Console.ReadLine();
+
+            //check the user input and restart from the top again
+            if (input.ToUpper() == "Y")
+            {
+                Console.Clear(); goto Restart;
+            }
+            else { Console.WriteLine("------------END!!!------------"); }
+                
+
+        }
+
+        static string NumToRoman(int number) 
+        {
+
+            Dictionary<string, int> RomanNums = new Dictionary<string, int>
+            {
+                {"M", 1000 },
+                {"CM", 900 },
+                {"D", 500 },
+                {"CD", 400 },
+                {"C", 100 },
+                {"XC", 90 },
+                {"L", 50 },
+                {"XL", 40 },
+                {"X", 10 },
+                {"IX", 9 },
+                {"V", 5 },
+                {"IV", 4 },
+                {"I", 1 }
+            };
+
+            // an empty string variable to store the roman numerals
+            string Result = "";
+
+            // goes through the dictionary
+            foreach (var rom in RomanNums)
+            {
+                // stops the program from going to negative numbers
+                // and stops it from printing the wrong roman numeral
+                while (number >= rom.Value)
+                {
+                    // saves the roman numeral('M', 'X', 'I', etc...) in the "Result" string
+                    Result += rom.Key;
+                    // makes "number" smaller so the program finishes 
+                    // and stop the program to keep printing the same roman numeral 
+                    number -= rom.Value;
+                }
+            }
+
+            // return the result(roman numerals)
+            return Result;
+        }
+    }
+}
